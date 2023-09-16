f65fb0b52d0c90b15535d9bf16ba41f298924a80
stjason022 <stjason022@student.wethinkcode.co.za>
2023-09-16 16:37:25 +0200

    test

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+
+Bun.serve({
+    fetch(req){
+        const url = new URL(req.url);
+        // test path
+        if (url.pathname === "/test1"){
+            return new Response("response from test 1")
+        }
+
+        if (url.pathname === "/test2"){
+            return new Response("response from test 2")
+        }
+        return new Response("404!")
+    }
+}
+   
+)
